4e8d06db1494a15ea654e19bb17af4896e28a1a3
Joonas Ahovalli <9369455+joonas-a@users.noreply.github.com>
2023-10-02 11:10:17 +0300

    add cypress v2

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,9 +1,15 @@
 describe('Pokedex', function () {
   it('front page can be opened', function () {
-    cy.visit('http://localhost:5000');
-    cy.contains('ivysaur');
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur')
     cy.contains(
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
-    );
-  });
-});
+    )
+  })
+  it('user can navigate to page of a pokemon', function () {
+    cy.visit('http://localhost:5000')
+    const ivysaur = cy.contains('ivysaur')
+    ivysaur.click()
+    cy.contains('chlorophyll')
+  })
+})
